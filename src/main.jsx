69f07c5075608b8443bfb0e0345188ac07088fcb
Joseph Reyes <jreyes1919@gmail.com>
2024-06-01 11:06:22 -0500

    took out GA for testing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 // import App from "./App.jsx";
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
 import "./index.css";
 import {
   HomePage,
@@ -17,17 +17,17 @@ import Footer from "./components/Footer";
 
 // Google Analytics
 
-ReactGA.initialize("G-FY0BX8EQQQ");
+// ReactGA.initialize("G-FY0BX8EQQQ");
 
-ReactGA.send({ hitType: "pageview", page: "/", title: "Home Page" });
-ReactGA.send({ hitType: "pageview", page: "/about", title: "About Page" });
-ReactGA.send({ hitType: "pageview", page: "/services", title: "Service Page" });
-ReactGA.send({
-  hitType: "pageview",
-  page: "/portfolio",
-  title: "Portfolio Page",
-});
-ReactGA.send({ hitType: "pageview", page: "/contact", title: "Contact Page" });
+// ReactGA.send({ hitType: "pageview", page: "/", title: "Home Page" });
+// ReactGA.send({ hitType: "pageview", page: "/about", title: "About Page" });
+// ReactGA.send({ hitType: "pageview", page: "/services", title: "Service Page" });
+// ReactGA.send({
+//   hitType: "pageview",
+//   page: "/portfolio",
+//   title: "Portfolio Page",
+// });
+// ReactGA.send({ hitType: "pageview", page: "/contact", title: "Contact Page" });
 
 // router
 const router = createBrowserRouter([
